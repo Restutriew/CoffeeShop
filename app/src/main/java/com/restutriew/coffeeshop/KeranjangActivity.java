ec9060cdc44fa35a8366475bca3e41f84c898ed2
Restu <restutriew@gmail.com>
2019-08-19 15:52:05 +0700

    Sampai tampilan struk

--- app/src/main/java/com/restutriew/coffeeshop/KeranjangActivity.java
+++ app/src/main/java/com/restutriew/coffeeshop/KeranjangActivity.java
@@ -1,11 +1,13 @@
 package com.restutriew.coffeeshop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class KeranjangActivity extends AppCompatActivity {
 
@@ -17,6 +19,16 @@ public class KeranjangActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setLogoDescription(getResources().getString(R.string.app_name));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button button = findViewById(R.id.proses);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent keranjang = new Intent(KeranjangActivity.this, PembayaranActivity.class);
+                startActivity(keranjang);
+            }
+        });
+
     }
 
 }
